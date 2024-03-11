fc197a2d690dc1b2f5b99511cefb0accd2c19e1b
Guilherme Vieira <guilhermeharfy@gmail.com>
2024-03-10 22:24:07 -0400

    Controllers, database, tests, frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1 do
       resources :records
@@ -7,5 +8,12 @@ Rails.application.routes.draw do
   end
 
   root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+
+  # Logout method
+  delete "signin", controller: :signin, action: :destroy
 
 end
