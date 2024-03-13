f895f1278aa103dc2de71d012adf52feb6265039
guilherme vieira <guilherme.vieira@agetec.campogrande.ms.gov.br>
2024-03-13 13:22:29 -0400

    :Layout:

---
+++ frontend/src/components/Signin.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable no-trailing-spaces -->
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+  <div class="max-w-sm m-auto my-8">
+
+  </div>
+</template>
+
+<script>
+
+</script>
